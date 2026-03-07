9d81f9c78d98f424caf7e16910e4c078d7c8ad5a
Yash <raunakyash12@gmail.com>
2026-03-07 12:04:57 +0530

    fixed audio play bar

--- Spotifiy-Backend/Frontend/src/pages/Browse.tsx
+++ Spotifiy-Backend/Frontend/src/pages/Browse.tsx
@@ -146,7 +146,7 @@ useEffect(() => {
         </div>
 
         {/* Music Grid */}
-        <section>
+        <section
           <SectionHeader title="All Music" />
           <div className="grid grid-cols-2 gap-5 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5">
             {allMusic.map((track, i) => (
